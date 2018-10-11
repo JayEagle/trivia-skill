5e5c5f65c42ab16139fdde03daad3689d97691b4
JayEagle <41083716+JayEagle@users.noreply.github.com>
2018-10-11 12:09:24 -0500

    Remove getlogger call

--- __init__.py
+++ __init__.py
@@ -10,8 +10,6 @@ from mycroft.util.log import LOG
 
 __author__ = 'j1bla02'
 
-# LOG = getLogger(__name__)
-
 # Each skill is contained within its own class, which inherits base methods
 # from the MycroftSkill class.  You extend this class as shown below.
 
